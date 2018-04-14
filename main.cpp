363aa89e3e56947d1ef102170aabe4d14ba40655
Tishkovec E.V <tiishka1@mail.ru>
2018-04-14 22:18:09 +0300

    Add comments to main

--- main.cpp
+++ main.cpp
@@ -5,47 +5,61 @@
 
 int main()
 {   
+    // Создание глобальной карты
     GlobalMap* GM;
+    // В аргемунте конструктора - размер карты
     GM = new GlobalMap(20);
-    GM->setGlobalMap();    
+    GM->setGlobalMap();
 
+    // Создание навигатора
     Navigator* NV;
     NV = new Navigator(GM);
+    // Установить текущее местоположение
     NV->setCoordinates(18, 1);
 
+    // Создание робота
     Robot* robotic;
+    // Загружаем в робота навигатор
     robotic = new Robot(NV);
 
+    // Создание открытой карты
     OpenMap* OM;
+    // Создание визуализации
     Display* DISP;
     DISP = new Display();
+
+    // Получение текущей открытой карты
     OM = robotic->showSituation();
+    // Отображение
     DISP->display(OM);
 
     while(1){
         char ch;
         cin >> ch;
 
-
         switch(ch){
         case 'w': {
+            // Робот идет вперед
             robotic->goUp();
             break;}
         case 'a': {
+            // Робот идет влево
             robotic->goLeft();
             break;}
         case 's': {
+            // Робот идет вниз
             robotic->goDown();
             break;}
         case 'd': {
+            // Робот идет вправо
             robotic->goRight();
             break;
         }
         }
+        // Получение текущей открытой карты
         OM = robotic->showSituation();
+        // Отображение
         DISP->display(OM);
-
-
     }
     return 0;
 }
